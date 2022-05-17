2803ee583f27550c8128a714b56d70538bc41a6f
vnonline2008 <vnonline2008@gmail.com>
2022-05-17 10:23:54 +0700

    Trigger Travis CI

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=tungtt44
+export POSTGRES_PASSWORD=TungTT44
+export POSTGRES_HOST=pgr-398094323019.cpflzbs6bneb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3-398094323019-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=tungtt44
+export URL=http://localhost:8100
